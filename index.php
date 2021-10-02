8b71f521464a5e6a12378bd7f6ef54c89adfb87f
Angelus123 <izerefaifelix@gmail.com>
2021-10-03 00:36:54 +0200

    Initial start up code

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Abagroup</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="ABAGROUP">
+	<meta name="keywords" content="IZERE, unica, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="Styles.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+	
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="header-top">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-lg-12 text-lg-right header-top-right">
+						<div class="top-social">
+							
+							<a href="#"> <i><img style=" border: 1px solid orangered; "src="img/language.svg" alt=""> Language</i></a>
+							<a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+							<a href="https://www.twitter.com/"><i class="fa fa-twitter"></i></a>
+							<a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>	
+							<a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
+								
+						</div>
+
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		<div >
+			<div >
+				<div >
+					<div class="site-navbar">
+						<a href="#" class="site-logo"><img src="img/logo1.png" alt=""></a>
+						<div class="nav-switch">
+							<i class="fa fa-bars"></i>
+						</div>
+						<div class="nav-search">
+							<input type="search" name="search" placeholder="Search for property"/>
+						</div>
+						
+						<ul class="main-menu">
+							<li><a href="index.html">HOME</a></li>
+							<li><a href="about.html">ABOUT US</a></li>
+							<li><a href="contact.html">CONTACT US</a></li>
+							<li><a href="library.html">LIBRARY</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Hero section -->
+	<div class="container-hero">
+		<div class="hero-text" >
+			<img class="hero-pic" src="img/1.jpg" alt="" />
+			<h4>Find your place with our local life style</h4>
+			<p>Search real estate property records, houses, land and more on Abagroup.<br>
+			Find property info from the most comprehensive source data.</p>
+		</div>
+		<div class=" hero-text" >
+			<img class="hero-pic" src="img/2.jpg" alt="" />
+			<h4>Find your place with our local life style</h4>
+			<p>Search real estate property records, houses, land and more on Abagroup.<br>
+			Find property info from the most comprehensive source data.</p>
+		</div>
+		<div class=" hero-text" >
+			<img class="hero-pic" src="img/about.jpg" alt="" />
+			<h4>Find your place with our local life style</h4>
+			<p>Search real estate property records, houses, land and more on Abagroup.<br>
+			Find property info from the most comprehensive source data.</p>
+		</div>
+
+	</div>
+	<!-- Hero section end -->
+	<!-- Services section -->
+	<section class="services-section spad set-bg" data-setbg="img/service-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">				
+					<img src="img/service.jpg" alt="">
+				</div>
+				<div class="col-lg-5 offset-lg-1 pl-lg-0">
+					<div class="section-title text-white">
+						<h3>OUR SERVICES</h3>
+						<p>We provide the perfect service for </p>
+					</div>
+					<div class="services">
+						<div class="service-item">
+						<i class="fa fa-comments"></i>
+								<div class="service-text">
+								<h5>Property for Rent, Buy & Sell</h5>
+								<p>We provide you with the best services which is best for your family 
+									and which suits your pocket.</p>
+							</div>
+						</div>
+						<div class="service-item">
+							<i class="fa fa-home"></i>
+							<div class="service-text">
+								<h5>Properties Management</h5>
+								<p>We manage your property considering as our own and give you the best possible solution regarding it.</p>
+							</div>
+						</div>
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>Renting and Selling</h5>
+								<p>Enjoy various services provided by us without any mid-man, Book your dream home today!.</p>
+							</div>
+						</div>
+
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>Property Valuation</h5>
+								<p>Enjoy various services provided by us </p>
+							</div>
+						</div>
+
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>Building Project Estimate</h5>
+								<p>Enjoy various services provided by us </p>
+							</div>
+						</div>
+
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>Building Drawing Design</h5>
+								<p>Enjoy various services provided by us </p>
+							</div>
+						</div>
+
+						<div class="service-item">
+							<i class="fa fa-briefcase"></i>
+							<div class="service-text">
+								<h5>ICT Computer Service Activities</h5>
+								<p>Enjoy various services provided by us </p>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Services section end -->
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<h3>LATEST NEWS</h3>
+				<p>Real estate news headlines around Rwanda.</p>
+			</div>
+			<div class="row">
+				
+		
+				<div class="col-lg-4 col-md-6 blog-item">
+					<img src="img/blog/3.jpg" alt="">
+					<h5><a href="#">Kigali  market saturated with inventory, says celebrity realtor</a></h5>
+					<div class="blog-meta">
+						<span><i class="fa fa-user"></i>Muhanga </span>
+						<span><i class="fa fa-clock-o"></i>14 June 2021</span>
+					</div>
+					<p>Integer luctus diam ac scerisque consectetur. Vimus dotnetact euismod lacus sit amet. Aenean interdus mid vitae maximus...</p>
+				</div>
+
+
+
+				<div class="col-lg-4 col-md-6 blog-item">
+					<img src="img/blog/3.jpg" alt="">
+					<h5><a href="#">Kigali  market saturated with inventory, says celebrity realtor</a></h5>
+					<div class="blog-meta">
+						<span><i class="fa fa-user"></i>Muhanga </span>
+						<span><i class="fa fa-clock-o"></i>14 June 2021</span>
+					</div>
+					<p>Integer luctus diam ac scerisque consectetur. Vimus dotnetact euismod lacus sit amet. Aenean interdus mid vitae maximus...</p>
+				</div>
+
+
+				<div class="col-lg-4 col-md-6 blog-item">
+					<img src="img/blog/3.jpg" alt="">
+					<h5><a href="#">Kigali  market saturated with inventory, says celebrity realtor</a></h5>
+					<div class="blog-meta">
+						<span><i class="fa fa-user"></i>Muhanga </span>
+						<span><i class="fa fa-clock-o"></i>14 June 2021</span>
+					</div>
+					<p>Integer luctus diam ac scerisque consectetur. Vimus dotnetact euismod lacus sit amet. Aenean interdus mid vitae maximus...</p>
+				</div>
+
+
+			</div>
+		</div>
+	</section>
+	<!-- Blog section end -->
+
+	<!-- Clients section -->
+	<div class="clients-section">
+		<div class="container">
+			<div class="clients-slider owl-carousel">
+				<a href="#">
+					<img src="img/partner/1.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/2.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/3.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/4.png" alt="">
+				</a>
+				<a href="#">
+					<img src="img/partner/5.png" alt="">
+				</a>
+			</div>
+		</div>
+	</div>
+	<!-- Clients section end -->
+
+
+
+	<!-- Footer section -->
+	<footer class="footer-section set-bg" data-setbg="img/footer-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 footer-widget">
+					<img src="img/logo1.png" alt="">
+					<p>We provide you with the best services which is best for your family and which suits your pocket.</p>
+					<div class="social">
+						
+						<a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+							<a href="https://www.twitter.com/"><i class="fa fa-twitter"></i></a>
+							<a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
+							<a href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a>
+							
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-widget">
+					<div class="contact-widget">
+						<h5 class="fw-title">CONTACT US</h5>
+						<p><i class="fa fa-map-marker"></i>You can contact us here......  </p>
+						<p><i class="fa fa-phone"></i>+250785182823</p>
+						<p><i class="fa fa-envelope"></i>info.abgroup@gmail.com</p>
+						<p><i class="fa fa-clock-o"></i>Mon - Sat, 08 AM - 06 PM</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-widget">
+					<div class="double-menu-widget">
+						<h5 class="fw-title">POPULAR PLACES</h5>
+						<ul>
+							<li><a href="">Kigali</a></li>
+							<li><a href="">MUSANZE</a></li>
+							<li><a href="">RUBAVU</a></li>
+
+
+						
+						</ul>
+						<ul>
+							<li><a href="">RUSIZI</a></li>
+							<li><a href="">RWAMAGANA</a></li>
+							<li><a href="">NYAGATARE</a></li>
+						
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6  footer-widget">
+					<div class="newslatter-widget">
+						<h5 class="fw-title">NEWSLETTER</h5>
+						<p>Subscribe your email to get the latest news and new offer also discount</p>
+						<form class="footer-newslatter-form">
+							<input type="text" placeholder="Email address">
+							<button><i class="fa fa-send"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</footer>
+
+	<!-- Footer section end -->
+                                        
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/masonry.pkgd.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
